7da7b0c0449315337eef0cdb4b9dc4f35a8ef7cd
Bernold <bernoldaz@gmail.com>
2020-07-06 22:03:41 -0600

    Vamos arturo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,11 @@ const routes: Routes = [
   {
     path: 'inicio',
     loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
+  },  {
+    path: 'principal',
+    loadChildren: () => import('./principal/principal.module').then( m => m.PrincipalPageModule)
   }
+
 
 
 ];
