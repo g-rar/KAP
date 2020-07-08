72b8c87e026974dce038e8f81a459fe1afd54224
Gerardo López Calderón <42049053+g-rar@users.noreply.github.com>
2020-07-07 22:09:52 -0600

    stuff not working right now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,12 @@ import { Component, OnInit } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+// import { Web3Service } from './util/web3.service';
+
+
+declare let require: any;
+const metacoin_artifacts = require('../../build/contracts/TestContract.json');
+
 
 @Component({
   selector: 'app-root',
@@ -48,7 +54,8 @@ export class AppComponent implements OnInit {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    // private w3 : Web3Service
   ) {
     this.initializeApp();
   }
@@ -60,10 +67,26 @@ export class AppComponent implements OnInit {
     });
   }
 
+
+  MetaCoin : any;
+
   ngOnInit() {
     const path = window.location.pathname.split('folder/')[1];
     if (path !== undefined) {
       this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
     }
+
+    //pruebas
+    // this.w3.artifactsToContract(metacoin_artifacts)
+    // .then((MetaCoinAbstraction) => {
+    //   this.MetaCoin = MetaCoinAbstraction;
+    //   this.MetaCoin.deployed().then(deployed => {
+    //     console.log(deployed);
+    //     deployed.Transfer({}, (err, ev) => {
+    //       console.log('Transfer event came in, refreshing balance');
+    //     });
+    //   });
+
+    // });
   }
 }
